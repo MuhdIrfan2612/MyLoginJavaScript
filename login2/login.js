f0201b0517e5f220043867c614e7a9814ecfe986
MuhdIrfan2612 <59907038+MuhdIrfan2612@users.noreply.github.com>
2020-02-12 21:55:53 +0800

    Add files via upload

---
+++ login2/login.js
@@ -0,0 +1,11 @@
+<script>
+        var count = 2;
+
+        function validate()
+        {
+            var un = document.login.username.value;
+            var pw = document.login.password.value;
+            var valid = false
+        }
+
+        </script>
